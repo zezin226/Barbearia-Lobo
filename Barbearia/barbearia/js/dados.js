647ff39175db3db16d4c0d03bed1d95ca89f350c
zezin226 <josearthuraraujolopes@gmail.com>
2026-05-27 10:54:23 -0300

    Add files via upload

---
+++ Barbearia/barbearia/js/dados.js
@@ -0,0 +1,53 @@
+// js/dados.js
+
+const ITENS = [
+  { 
+    nome: "Corte Clássico",
+    descricao: "Escolha o corte de sua preferência.",
+    preco: "R$ 25,00",
+    imagem: "../img/modelo.png", // Caminho corrigido
+    
+  },
+  { 
+    nome: "Barba", 
+    descricao: "Faça sua barba com a gente.", 
+    preco: "R$ 20,00", 
+    imagem: "img/americano.png" 
+  },
+  { 
+    nome: "Combo Corte+Barba", 
+    descricao: "combo barato de corte e barba, leve 2 pague 1.", 
+    preco: "R$ 35,00", 
+    imagem: "img/americano.png" 
+  },
+  { 
+    nome: "Pigmentação", 
+    descricao: "faça uma pigmentação com a gente e fique mais atraente.", 
+    preco: "R$ 40,00", 
+    imagem: "img/americano.png" 
+  },
+  { 
+    nome: "combo corte+luzes", 
+    descricao: "combo barato de corte e luzes, leve 2 pague 1.", 
+    preco: "R$ 70,00", 
+    imagem: "img/americano.png" 
+  },
+  { 
+    nome: "progressiva (liso ou cacheado)", 
+    descricao: "tratamento de alisamento ou cacheio.", 
+    preco: "R$ 80,00", 
+    imagem: "img/americano.png" 
+  },
+  { 
+    nome: "luzes ou pintura", 
+    descricao: "faça luzes ou pinte seu cabelo com a gente.", 
+    preco: "R$ 50,00", 
+    imagem: "img/americano.png" 
+  },
+  { 
+    nome: "descoloração", 
+    descricao: "faça uma descoloração com a gente.", 
+    preco: "R$ 70,00", 
+    imagem: "img/americano.png" 
+  },
+];
